946bc6fde0e5ab889e924f11c40dc8486d5c1c62
Andreas Wagner <awagner@gmx.de>
2015-10-14 19:41:24 +0200

    Dokumentierte Version

--- app/src/main/java/de/dillingen/zahlenraten/Zahlenraten.java
+++ app/src/main/java/de/dillingen/zahlenraten/Zahlenraten.java
@@ -1,25 +1,21 @@
 package de.dillingen.zahlenraten;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.KeyEvent;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import java.util.Random;
 
-
 public class Zahlenraten extends ActionBarActivity {
 
     private int zahl;
     private int versuche = 0;
-
     private Random zufallsgenerator;
 
-    // Referenzen auf die Viewobject
+    // Referenzen auf die Views
     private EditText inputnumber;
     private TextView status;
 
@@ -31,35 +27,38 @@ public class Zahlenraten extends ActionBarActivity {
 
         zufallsgenerator = new Random();
 
-        // Referenzen der ViewObjecte holen.
-        prepareViewObjects();
+        // Referenzen der Views holen.
+        inputnumber = (EditText) this.findViewById(R.id.inputnumber);
+        status = (TextView) this.findViewById(R.id.status);
 
         // Spiel starten.
         newGame();
     }
 
-    /** holt die Referenzen auf die Viewobjekte */
-    protected void prepareViewObjects() {
-        inputnumber = (EditText) this.findViewById(R.id.inputnumber);
-        status = (TextView) this.findViewById(R.id.status);
-    }
-
-    /** startet ein neues Spiel */
+    /**
+     * Startet ein neues Spiel
+     */
     public void newGame() {
 
         zahl = zufallsgenerator.nextInt(101);
         status.setText("Raten Sie eine Zahl zwischen 0 und 100!");
     }
 
-    /** überprüft die eingegebene Zahl */
+    /**
+     * Überprüft die eingegebene Zahl
+     */
     private void checkNumber() {
 
+        // Die eingegebene Zahl auslesen.
         int currentnumber = Integer.parseInt(inputnumber.getText().toString());
         versuche++;
 
         if (currentnumber == zahl) {
+
             status.setText("Sie haben die Zahl erraten!\nVersuche: " + versuche);
+
         } else {
+
             if (currentnumber < zahl) {
                 status.setText(currentnumber + " ist zu klein!\nVersuche: " + versuche);
                 inputnumber.setText("");
@@ -68,45 +67,31 @@ public class Zahlenraten extends ActionBarActivity {
                 inputnumber.setText("");
             }
         }
-
-
     }
 
+    /**
+     * Falls bei der Tastatureingabe die Enter-Taste gedrückt wird, die Nummer prüfen
+     * und die Tastatur schließen (return true).
+     *
+     * @param keyCode, die Codierungszahl für die verschiedenen Tasten
+     * @param event,   Zusatz-Parameter für den Event
+     * @return true,   falls die Berechnung durchgeführt wurde und die Tastatur geschlossen
+     * werden soll.
+     */
     @Override
     public boolean onKeyUp(int keyCode, KeyEvent event) {
 
-        if (keyCode ==  KeyEvent.KEYCODE_ENTER) {
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
             this.checkNumber();
             return true;
         }
         return super.onKeyUp(keyCode, event);
     }
 
-
-    /** Ereignisbehandlung des "starteSpiel" Buttons: startet das Spiel */
+    /**
+     * Startet das Spiel (Ereignisbehandlung des "starteSpiel" Buttons)
+     */
     public void onClickNewGame(View v) {
         this.newGame();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_zahlenraten, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
